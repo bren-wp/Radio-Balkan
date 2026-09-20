008a34827a9eea47099b8b509f0fe312f27d6e78
Brendigo <info@brendigo.com>
2026-09-21 00:34:45 +0200

    release: bump Android AppInfo version

--- apps/android/app/src/main/java/net/radiobalkan/app/AppInfo.java
+++ apps/android/app/src/main/java/net/radiobalkan/app/AppInfo.java
@@ -2,7 +2,7 @@ package net.radiobalkan.app;
 
 /** Central production metadata used by network clients and UI. */
 public final class AppInfo {
-    public static final String VERSION = "0.0.32";
+    public static final String VERSION = "0.0.33";
     public static final String USER_AGENT = "RadioBalkan-Android/" + VERSION;
 
     private AppInfo() { }
