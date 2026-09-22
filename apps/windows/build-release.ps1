a8ecc992a30d0690511ea2813c18da92144a73a7
Brendigo <info@brendigo.com>
2026-09-23 01:46:17 +0200

    release: bump Radio Balkan to v0.0.44

--- apps/windows/build-release.ps1
+++ apps/windows/build-release.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$Version = "0.0.43",
+    [string]$Version = "0.0.44",
     [string]$Output = (Join-Path $PSScriptRoot "dist"),
     [string]$SigningThumbprint = $env:RADIO_BALKAN_SIGNING_THUMBPRINT,
     [string]$TimestampUrl = "http://timestamp.digicert.com",
